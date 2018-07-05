72707713a9ecdb2c4dde255bbb220931197109de
Kyle Bashour <kylebshr@me.com>
2018-07-05 14:51:33 -0700

    Hide tab bar

--- RoundCorners/DisplayCornersViewController.swift
+++ RoundCorners/DisplayCornersViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class XCornersViewController: UIViewController {
+class DisplayCornersViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var continuousSwitch: UISwitch!
 
+    private var tabBar: UITabBar {
+        return tabBarController!.tabBar
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        toggleTabBar()
         updateCorners()
     }
 
@@ -23,5 +28,13 @@ class XCornersViewController: UIViewController {
         let value = round(slider.value * 10) / 10
         label.text = "\(value)"
         view.roundCorners(radius: CGFloat(value), continuous: continuousSwitch.isOn)
+    }
+
+    @IBAction func toggleTabBar() {
+        if tabBar.frame.origin.y == view.frame.height {
+            tabBar.frame.origin.y = view.frame.height - tabBar.frame.height
+        } else {
+            tabBar.frame.origin.y = view.frame.height
+        }
     }
 }
